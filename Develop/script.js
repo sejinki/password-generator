04ad653c2d78459474ea0aa6cd9a75c3924bf02c
Sejin <kisejin2015@gmail.com>
2022-06-27 13:40:29 -0600

    variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var choiceArray = []
+var characterLength = 8;
+
+var number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ];
+var specialCharacter= [ '!','@','#','$','%','^','&','*','(',')','-','_','+','=' ];
+var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
+var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
